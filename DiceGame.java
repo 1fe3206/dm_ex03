bee83cdaaa67e71f27c33ffbd9a7d029b850d538
1fe3206 <tetsu.k.1230@icloud.com>
2025-06-27 12:23:13 +0900

    Dice game: initial revision

---
+++ DiceGame.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class DiceGame{
+    public static void main(String args[]){
+        Random rand = new Random();
+        int x = rand.nextInt(6) + 1;
+        int y = rand.nextInt(6) + 1;
+
+        System.out.println("Rolling dice...");
+        System.out.println("Die 1: " + x);
+        System.out.println("Die 2: " + y);
+        System.out.println("Total value: " + (x+y));
+    }
+}
